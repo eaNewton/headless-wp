dd4b3c6dbda01130c6edd52a01f13ff8eea01dca
eaNewton <emmett.newton@praxent.com>
2019-03-16 16:10:38 -0500

    add fonts, update gitignore, functions.php TOC

--- wp-content/themes/wasabi/functions.php
+++ wp-content/themes/wasabi/functions.php
@@ -1,7 +1,19 @@
 <?php
+/**
+ * Table of Contents
+ *
+ * 1. Action addition/removal
+ * 2. Enqueue Resources
+ */
 
+/**
+ * 1. Action addition/removal
+ */
 add_action( 'wp_enqueue_scripts', 'wasabi_enqueue_styles' );
 
+/**
+ * 2. Enqueue Resources
+ */
 function wasabi_enqueue_styles() {
 	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 }
